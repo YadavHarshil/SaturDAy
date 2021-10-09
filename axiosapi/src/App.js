867c2cbcf00d00fe7a21b3e85c270ccf199bf784
harshil-yadav <harshil.yadav@green-apex.com>
2021-10-09 09:00:54 +0530

    Axios

---
+++ axiosapi/src/App.js
@@ -0,0 +1,17 @@
+// import PageBootsrap from "./PageBootsrap.js";
+import AxiosPagination from "./AxiosPagination.js";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Test from "./Test.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AxiosPagination />
+      </header>
+    </div>
+  );
+}
+
+export default App;
